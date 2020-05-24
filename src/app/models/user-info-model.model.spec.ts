33b37d98c4fe26469505e5f5aa4e40b36553a5a3
bullet888 <bullet888@gmail.com>
2020-05-24 13:20:08 +0400

    push initial project to azure

---
+++ src/app/models/user-info-model.model.spec.ts
@@ -0,0 +1,7 @@
+import { UserInfoModel } from './user-info-model.model';
+
+describe('UserInfoModel', () => {
+  it('should create an instance', () => {
+    expect(new UserInfoModel()).toBeTruthy();
+  });
+});
---
+++ src/app/models/user-info-model.model.ts
@@ -0,0 +1,22 @@
+export class UserInfoModel
+{
+	guid: string;
+	customerUid: string;
+	
+	first_name: string;
+	last_name: string;
+
+	email: string;
+	zipcode: string;
+
+	password: string;
+
+	constructor(obj: any = null)
+	{
+		if(obj != null)
+		{  
+			Object.assign(this, obj);
+			this.customerUid = obj.uid;
+		}
+	}
+}
