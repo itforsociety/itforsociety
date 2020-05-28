9c67dfc97f41c544d8a6f6257cdc880c7dd0a85d
bullet888 <bullet888@gmail.com>
2020-05-28 20:15:44 +0400

    installing jquery

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { mixinColor } from '@angular/material/core';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,9 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+   //$("#header").css("background-color", "blue");
   }
+
+  
 
 }
