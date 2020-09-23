6345c13201b3eb5e2e124194358206b5caafbd0e
bullet888 <bullet888@gmail.com>
2020-09-23 12:01:28 +0400

    before using story model

---
+++ src/app/models/story.model.ts
@@ -0,0 +1,19 @@
+export class Story {
+    story_id: number;
+	
+	story_title: string;
+	story_subtitle: string;
+
+	story_avatar: string;
+	story_image: string;
+
+	story_content: string;
+
+	constructor(obj: any = null)
+	{
+		if(obj != null)
+		{  
+			Object.assign(this, obj);
+		}
+	}
+}
--- src/app/stories/stories.component.ts
+++ src/app/stories/stories.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Story} from '../models/story.model' 
 
 @Component({
   selector: 'app-stories',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stories.component.css']
 })
 export class StoriesComponent implements OnInit {
+
+  story: Story = new Story ({
+    story_id: 1,
+    story_title: 'Child Protection Information Management System (CPIMS)',
+    story_subtitle: 'Primero',
+    story_avatar: '../../assets/IT for society circle.png',
+    story_image: '../../assets/IT for society circle.png',
+    story_content: 'As of May 2018, 68.5 million people around the world have been forcibly displaced from their homes due to armed conflict and natural disaster.  🌪🔥💔'+
+    'Of these, 25.4 million are refugees, and more than 50 percent are children 🙅‍♀️🙅🙇‍♀️🙇‍♂️'+
+    'For helping missing people International Rescue Committee , Save the Children UK and UNICEF developed child protection information management system (CPIMS). 👨‍👩‍👦👨‍👩‍👧👨‍👩‍👧‍👦'+
+    'To know more about Primero visit primero.org '
+  });
 
   constructor() { }
 
