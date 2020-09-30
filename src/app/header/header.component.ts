002b97b19861c97a014e7f650c4a46249884a749
bullet888 <bullet888@gmail.com>
2020-09-30 18:03:34 +0400

    slideshow desktop

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,13 +8,27 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   constructor() { }
+  darkModeChecked = true;
+  darkThemeName = "my-pink-bluegrey";
+  lightThemeName = "my-indigo-pink";
 
   ngOnInit(): void {
   }
 
-  changeTheme(themeName) {
+/*   changeTheme(themeName) {
    var elemCSS =  document.getElementById('themeAsset');
    elemCSS.setAttribute ('href','../assets/css/' + themeName + '.css');
+  } */
+
+  darkMode(){
+    var elemCSS =  document.getElementById('themeAsset');
+    if (this.darkModeChecked){
+      console.log(this.darkThemeName)
+      elemCSS.setAttribute ('href','../assets/css/' + this.darkThemeName + '.css');
+    }
+    else{
+      elemCSS.setAttribute ('href','../assets/css/' + this.lightThemeName + '.css');
+    }
   }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,9 +9,81 @@ import { mixinColor } from '@angular/material/core';
 export class HomeComponent implements OnInit {
 
   constructor() { }
+  images = [
+    {
+      imageID: 1,
+      imageURL: "../../assets/slideshow/large/1.jpg",
+      imageThumbURL: "../../assets/slideshow/thumbs/1.jpg"
+    },
+    {
+      imageID: 2,
+      imageURL: "../../assets/slideshow/large/2.jpg",
+      imageThumbURL: "../../assets/slideshow/thumbs/2.jpg"
+    },
+    {
+      imageID: 3,
+      imageURL: "../../assets/slideshow/large/3.jpg",
+      imageThumbURL: "../../assets/slideshow/thumbs/3.jpg"
+    }
+  ]
+  firstImage = this.images[0];
+  lastImage = this.images[this.images.length - 1];
+  selectedImage = this.firstImage;
+  nextImage = this.images[1];
+  previousImage = this.lastImage;
 
   ngOnInit(): void {
    //$("#header").css("background-color", "blue");
+  }
+
+  
+
+  nextSlide(imageID){
+    let index = this.images.findIndex(function(item, i){
+      return item.imageID === imageID
+    });
+    
+    //if it's last image get the first one.
+    if (index + 1 == this.images.length){
+      this.selectedImage = this.firstImage;
+    }
+    else{
+      this.selectedImage = this.images[index + 1];
+    }
+
+    
+    if (index == this.images.length - 2){
+      this.nextImage = this.firstImage;
+    }
+    else if (index == this.images.length - 1){
+      this.nextImage = this.images[1];
+    }
+    else{
+      this.nextImage = this.images[index + 2];
+    }
+  }
+
+  previousSlide(imageID){
+    let index = this.images.findIndex(function(item, i){
+      return item.imageID === imageID
+    });
+    //if it's first image get the last one.
+    if (index == 0){
+      this.selectedImage = this.lastImage;
+    }
+    else{
+      this.selectedImage = this.images[index - 1];
+    }
+
+    if (index == 0){
+      this.previousImage = this.images[this.images.length - 2];
+    }
+    else if (index == 1){
+      this.previousImage = this.lastImage;
+    }
+    else{
+      this.previousImage = this.images[index-2];
+    }
   }
 
   
