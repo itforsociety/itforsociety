544962b7c6ecf018cf6fc2294b20b26101efd78f
bullet888 <bullet888@gmail.com>
2022-07-14 17:34:23 +0200

    background scrolling transition

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -9,6 +9,18 @@ export class ProductsComponent implements OnInit {
 
   constructor() { }
 
+  scrollEffect() {
+    const [red, green, blue] = [0, 35, 106]
+    //const [red2, green2, blue2] = [08, 213, 224]
+    const scrollElement = document.getElementsByClassName("productCards")[0];
+    const section1 = document.getElementById("elementsSVGFirst") as HTMLElement;
+    const section2 = document.getElementById("elementsSVGSecond") as HTMLElement;
+    const y = 1 - scrollElement.scrollTop / 850;
+    const x = 0 + scrollElement.scrollTop / 850;
+    section1.style.opacity = y.toString();
+    section2.style.opacity = x.toString();
+    console.log("<br>Vertically: " + y);
+  }
   ngOnInit(): void {
   }
 
