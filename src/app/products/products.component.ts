72be9e5f62bbadd226f9bb49a99aedad954528d3
bullet888 <bullet888@gmail.com>
2022-07-18 13:21:14 +0200

    adjusting footer and services page

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -7,19 +7,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    
+   }
+
+   scrollValue = 0;
+   matrixValue = -109.54
 
   scrollEffect() {
     const [red, green, blue] = [0, 35, 106]
+    const [m1, m2, m3, m4, m5, m6] = [(0.208 + (this.scrollValue/2)), (-0.978 + (this.scrollValue/2)) ,(0.978+ (this.scrollValue/2)) ,(0.708 + (this.scrollValue/2)) , (159.041 + (this.scrollValue/2)) ,  1056.417 ]
     //const [red2, green2, blue2] = [08, 213, 224]
     const scrollElement = document.getElementsByClassName("productCards")[0];
     const section1 = document.getElementById("elementsSVGFirst") as HTMLElement;
     const section2 = document.getElementById("elementsSVGSecond") as HTMLElement;
-    const y = 1 - scrollElement.scrollTop / 850;
-    const x = 0 + scrollElement.scrollTop / 850;
+    const Rectangle_1 = document.getElementById("Rectangle_1") as HTMLElement;
+    const Rectangle_2 = document.getElementById("Rectangle_2") as HTMLElement;
+    const Polygon_1 = document.getElementById("Polygon_1") as HTMLElement;
+    const Polygon_2 = document.getElementById("Polygon_2") as HTMLElement;
+    const Ellipse_1 = document.getElementById("Ellipse_1") as HTMLElement;
+    const Ellipse_2 = document.getElementById("Ellipse_2") as HTMLElement;
+    console.log("translate( " + (1288 + this.scrollValue/2) + ", " + (176+this.scrollValue/2) +")" + "<br>Vertically: " + scrollElement.scrollTop.toString() + "scrollValue" + this.scrollValue + "matrix(" + (0.208 + (this.scrollValue/2)) +", " + (-0.978 + (this.scrollValue/2)) + "," + (0.978+ (this.scrollValue/2)) + "," + (0.708 + (this.scrollValue/2)) + "," + (159.041 + (this.scrollValue/2)) + "," + 1056.417 +")");
+    if (this.matrixValue = 246.46){
+      //this.matrixValue = this.matrixValue - this.scrollValue/10;
+      this.matrixValue = -109.54 + this.scrollValue/2;
+    }
+    else{
+      this.matrixValue = -109.54 + this.scrollValue/2;
+    }
+    this.scrollValue = scrollElement.scrollTop;
+    var y = 1 - this.scrollValue/ 850;
+    var x = 0 + this.scrollValue / 850;
     section1.style.opacity = y.toString();
     section2.style.opacity = x.toString();
-    console.log("<br>Vertically: " + y);
+    Rectangle_1.style.transform = "matrix(0.995, 0.105, -0.105, 0.995, -131.031, " + this.matrixValue.toString() + ")";
+    Rectangle_2.style.transform = "matrix(0.995, 0.105, -0.105, 0.995, -131.031, " + this.matrixValue.toString() + ")";
+    Polygon_1.style.transform = "matrix(" + (0.208 + (this.scrollValue/9000)) +", " 
+                                          + (-0.978 + (this.scrollValue/9000)) + "," 
+                                          + (0.978+ (this.scrollValue/9000)) + ","
+                                          + (0.208 + (this.scrollValue/9000)) + "," 
+                                          + (159.041 + (this.scrollValue/900)) + "," 
+                                          + (1056.417+ (this.scrollValue/900)) +
+                                        ")";
+    Polygon_2.style.transform = "matrix(" + (0.208 + (this.scrollValue/9000)) +", " 
+                                          + (-0.978 + (this.scrollValue/9000)) + "," 
+                                          + (0.978+ (this.scrollValue/9000)) + ","
+                                          + (0.208 + (this.scrollValue/9000)) + "," 
+                                          + (159.041 + (this.scrollValue/900)) + "," 
+                                          + (1056.417+ (this.scrollValue/900)) +
+                                        ")";
+    Ellipse_1.style.transform = "translate( " + (1288 - this.scrollValue/7) + "px, " + (176 - this.scrollValue/3) +"px)";
+    Ellipse_2.style.transform = "translate( " + (1288 - this.scrollValue/7) + "px, " + (176 - this.scrollValue/3) +"px)";
+    
+    
   }
   ngOnInit(): void {
   }
