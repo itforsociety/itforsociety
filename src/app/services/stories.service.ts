31cd64325150f96f14a04e4f5ae0c88a26b9a0c7
itforsociety <matej.bulic@itforsociety.com>
2026-01-29 18:26:14 +0100

    Revert "Fix: Update story and quote image paths to absolute paths with /itforsociety/ subdirectory"
    This reverts commit 4eff089b1c3d56b493ad6ebe49dcb0c0c2599dff.

--- src/app/services/stories.service.ts
+++ src/app/services/stories.service.ts
@@ -16,9 +16,9 @@ export class StoriesService {
     story_title: 'Share the Meal - The Nobel Peace Prize 2020',
     story_subtitle: 'World Food Program was awarded with Nobel peace prize for 2020',
     story_tag: 'wfp',
-    story_avatar: '/itforsociety/assets/IT for society circle.png',
-    story_image: '/itforsociety/assets/stories/5/wfpNobel.JPG',
-    story_og_image:'/itforsociety/assets/stories/5/wfpNobel.JPG',
+    story_avatar: '../../assets/IT for society circle.png',
+    story_image: '../../assets/stories/5/wfpNobel.JPG',
+    story_og_image:'assets/stories/5/wfpNobel.JPG',
     story_video_URL:'https://www.youtube.com/embed/efkVY-UyGDM',
     story_content: 'This month were Nobel Prize 2020 awards. Nobel prize for peace was awarded to World Food Programme (WFP). In 2019, the WFP provided assistance to 100 million people in 88 countries who are victims of acute food insecurity and hunger. They archived this, mostly with the help of Share the Meal application, which shows us another great example of IT for Society. Check the video how the Share the Meal application works and install the app to help battle against hunger. https://sharethemeal.org/en/index.html   '
   },{
@@ -27,9 +27,9 @@ export class StoriesService {
     story_title: 'Hello Tractor',
     story_subtitle: 'making affordable tractor services for the small farm holders which can not afford to buy one',
     story_tag: 'hellotractor',
-    story_avatar: '/itforsociety/assets/IT for society circle.png',
-    story_image: '/itforsociety/assets/stories/4/hellotractor.JPG',
-    story_og_image:'/itforsociety/assets/stories/4/hellotractor.JPG',
+    story_avatar: '../../assets/IT for society circle.png',
+    story_image: '../../assets/stories/4/hellotractor.JPG',
+    story_og_image:'assets/stories/4/hellotractor.JPG',
     story_video_URL:'https://www.youtube.com/embed/FZUilc5I0lw',
     story_content: 'You don\'t have a tractor? Don\'t worry you don\'t have to buy one, you can just rent it over "Hello Tractor"! Nigeria 🇳🇬     Great IT platform example in Nigeria 🇳🇬with positive impact on society, it\'s making affordable tractor services for the small farm holders which can\'t afford to buy one. Also great opportunity for Tractor owners to expand their business!         Connecting it with IoT and applications makes the perfect example of IT for Society!         Hello Tractor aims to transform         the agricultural ecosystem,         creating efficiencies         that generate inclusive growth,         reduce poverty,          and help build stronger, more stable economies         read more on:         https://hellotractor.com/ '
   },{
@@ -38,9 +38,9 @@ export class StoriesService {
     story_title: 'Create your website sitemap and robots.txt in Angular',
     story_subtitle: 'How does Google know about your website?',
     story_tag: 'sitemap',
-    story_avatar: '/itforsociety/assets/IT for society circle.png',
-    story_image: '/itforsociety/assets/stories/3/How does Google find my website.jpg',
-    story_og_image:'/itforsociety/assets/stories/3/How does Google find my website.jpg',
+    story_avatar: '../../assets/IT for society circle.png',
+    story_image: '../../assets/stories/3/How does Google find my website.jpg',
+    story_og_image:'assets/stories/3/How does Google find my website.jpg',
     story_video_URL:'false',
     story_content: 'Google searches your website with Web Crawler, also known as spider or spiderbot. When the website is found, it is starting to crawl your page. Google renders the page and analyses both the text and non-text content and overall visual layout to decide where it should appear in Search results.'
   },{
@@ -49,9 +49,9 @@ export class StoriesService {
     story_title: 'Child Protection Information Management System (CPIMS)',
     story_subtitle: 'Primero',
     story_tag: 'primero',
-    story_avatar: '/itforsociety/assets/IT for society circle.png',
-    story_image: '/itforsociety/assets/primero.PNG',
-    story_og_image:'/itforsociety/assets/primero.PNG',
+    story_avatar: '../../assets/IT for society circle.png',
+    story_image: '../../assets/primero.PNG',
+    story_og_image:'assets/primero.PNG',
     story_video_URL:'false',
     story_content: 'As of May 2018, 68.5 million people around the world have been forcibly displaced from their homes due to armed conflict and natural disaster.  🌪🔥💔'+
     'Of these, 25.4 million are refugees, and more than 50 percent are children 🙅‍♀️🙅🙇‍♀️🙇‍♂️'+
@@ -65,9 +65,9 @@ export class StoriesService {
     story_title: 'Sustainable Development Goals (SDGs)',
     story_subtitle: 'Goals established by United Nations',
     story_tag: 'sdg',
-    story_avatar: '/itforsociety/assets/SDG/SDG_Wheel_Transparent_WEB.png',
-    story_image: '/itforsociety/assets/SDG/SDG_Poster_2019_goals_WEB_transparent.png',
-    story_og_image:'/itforsociety/assets/SDG/SDG_Poster_2019_goals_WEB_transparent.png',
+    story_avatar: '../../assets/SDG/SDG_Wheel_Transparent_WEB.png',
+    story_image: '../../assets/SDG/SDG_Poster_2019_goals_WEB_transparent.png',
+    story_og_image:'assets/SDG/SDG_Poster_2019_goals_WEB_transparent.png',
     story_video_URL:'false',
     story_content: 'Sustainable Development Goals (SDGs) are the blueprint to achieve a better and more sustainable future for all. After concluding Millenium Development Goals agenda in 2015, United Nations created new agenda to achieve 17 goals by 2030. The 17 goals have 169 targets and 247 indicators  '
   }];
@@ -79,9 +79,9 @@ export class StoriesService {
     story_title: 'Jimi Hendrix',
     story_subtitle: 'quote',
     story_tag: 'jimi',
-    story_avatar: '/itforsociety/assets/IT for society circle.png',
-    story_image: '/itforsociety/assets/quotes/jimi hendrix when the power of love.jpg',
-    story_og_image:'/itforsociety/assets/quotes/jimi hendrix when the power of love.jpg',
+    story_avatar: '../../assets/IT for society circle.png',
+    story_image: '../../assets/quotes/jimi hendrix when the power of love.jpg',
+    story_og_image:'assets/quotes/jimi hendrix when the power of love.jpg',
     story_video_URL:'false',
     story_content: 'When the power of love overcomes the love of power, world will know peace'
   },{
@@ -90,9 +90,9 @@ export class StoriesService {
     story_title: 'Jordan Peterson',
     story_subtitle: 'quote',
     story_tag: 'peterson',
-    story_avatar: '/itforsociety/assets/IT for society circle.png',
-    story_image: '/itforsociety/assets/quotes/Jordan Peterson order your household.jpg',
-    story_og_image:'/itforsociety/assets/quotes/Jordan Peterson order your household.jpg',
+    story_avatar: '../../assets/IT for society circle.png',
+    story_image: '../../assets/quotes/Jordan Peterson order your household.jpg',
+    story_og_image:'assets/quotes/Jordan Peterson order your household.jpg',
     story_video_URL:'false',
     story_content: "Don't reorganize your state until you have ordered your own experience.Have some humility.If you cannot bring peace to your household,how dare you try to rule a city?"
   },{
@@ -101,9 +101,9 @@ export class StoriesService {
     story_title: 'Peter Maurer',
     story_subtitle: 'quote',
     story_tag: '4.0',
-    story_avatar: '/itforsociety/assets/IT for society circle.png',
-    story_image: '/itforsociety/assets/quotes/peter maurer qoute forth industrial revolution.jpg',
-    story_og_image:'/itforsociety/assets/quotes/peter maurer qoute forth industrial revolution.jpg',
+    story_avatar: '../../assets/IT for society circle.png',
+    story_image: '../../assets/quotes/peter maurer qoute forth industrial revolution.jpg',
+    story_og_image:'assets/quotes/peter maurer qoute forth industrial revolution.jpg',
     story_video_URL:'false',
     story_content: 'The Fourth Industrial Revolution has the potential to make inequalities visible and to make them less acceptable in the future'
   }]
